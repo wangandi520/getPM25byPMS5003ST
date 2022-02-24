71b3926cfcdfdce5c3d6f2e2541ff48fd9f5203f
wangandi520 <wangandi520@gmail.com>
2022-02-24 19:02:08 +0800

    Update receivePM2.5.php

--- receivePM2.5.php
+++ receivePM2.5.php
@@ -2,8 +2,8 @@
 header("Content-type:text/html;charset=utf-8");
 $pm = $_GET['pm'];
 $dsn = 'mysql:dbname=pm;host=127.0.0.1;port=3306';
-$username = 'root';
-$password = 'wozhiwni0';
+$username = '';
+$password = '';
 try {
     $pdo = new PDO($dsn, $username, $password);
 } catch(PDOException $e) {
