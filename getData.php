316e0d826c76cb473e028aacdb8e04ce309784e3
wangandi520 <wangandi520@gmail.com>
2022-02-24 19:20:23 +0800

    Update getData.php

--- getData.php
+++ getData.php
@@ -5,15 +5,15 @@ $password = '';
 try {
     $db = new PDO($dsn, $username, $password);
 } catch(PDOException $e) {
-    die('Could not connect to the database:
-' . $e);
+    die('Could not connect to the database:' . $e);
 }
 $db->query('set names utf8');
 $sql = "select * from pm order by time desc limit 1";
 $obj = $db->prepare($sql);
-$obj->execute(array(1));
+$obj->execute();
 $arr = $obj->fetchAll(PDO::FETCH_ASSOC);
 $data = array("pm"=>$arr[0]['data'],"date"=>$arr[0]['time']);
 echo json_encode($data);
 //print_r($arr[0][time]);
 ?>
+
